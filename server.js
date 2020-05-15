564d4a81abf1953b213f7f294fcbf096c9c05d0a
Michael Stallings <stall84@gmail.com>
2020-05-15 16:38:19 -0400

    checking to see if dotenv can be left on express server. if not, revert to commenting it out

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const db = require('./models');
-//require('dotenv').config();
+require('dotenv').config();
 
 const PORT = process.env.PORT;
 
