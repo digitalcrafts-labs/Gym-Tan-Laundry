d754984f187a70358af50d2e8a25fea03c00dc72
Michael Stallings <stall84@gmail.com>
2020-05-19 18:02:43 -0400

    created playlists model and added crystal's head content to header partial

---
+++ migrations/20200519062407-create-playlists.js
@@ -0,0 +1,34 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Playlists', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      playlistID: {
+        type: Sequelize.STRING
+      },
+      userID: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'Users',
+          key: 'id'
+        }
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Playlists');
+  }
+};
