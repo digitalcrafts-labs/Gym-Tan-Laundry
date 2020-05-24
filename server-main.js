33f647e712f4642059725f59471c0ec927954d3d
Michael Stallings <stall84@gmail.com>
2020-05-24 18:21:55 -0400

    Changed form values on home.ejs to capital gym/tan/laundry in order to dynamically create playlist names in server-main. created dynamic playlist names, edited CSS to put footer at bottom/absolute, edited the algorithm-functions to provide more diverse song returns

--- server-main.js
+++ server-main.js
@@ -199,12 +199,12 @@ app.post('/registration2', (req,res,next) => {
 })
 
 app.post('/modal-input', (req,res,next) => {
-    var playListType = req.body.playListType;
-    var songList = req.body.songList;
+     playListType = req.body.playListType;
+     songList = req.body.songList;
     
-    if(playListType === "gym") {
+    if(playListType === "GYM") {
         url = getRandomGymRecommendation(songList);
-    } else if (playListType === "tan") {
+    } else if (playListType === "TAN") {
         url = getRandomTanRecommendation(songList);
     } else {
         url = getRandomLaundryRecommendation(songList);
@@ -310,25 +310,25 @@ function getRandomGymRecommendation (length){
     const gymGenres = ["club", "dance", "happy", "hip-hop", "party", "drum-and-bass", "edm", "pop", "power-pop", "work-out"];
     let gymGenre = gymGenres[Math.floor(Math.random() * gymGenres.length)];
     let gymDanceability = (Math.random() * (0.3 - .9) + .9).toFixed(1);
-    let gymPopularity = (Math.random() * (50 - 100) + 100).toFixed(0);
-    let gymUrl = `https://api.spotify.com/v1/recommendations?limit=${length}&market=US&seed_genres=pop%2C%20${gymGenre}&target_danceability=${gymDanceability}&min_energy=0.4&target_popularity=${gymPopularity}&min_tempo=120&max_tempo=140&min_valence=.5`;
+    let gymPopularity = (Math.random() * (30 - 100) + 100).toFixed(0);
+    let gymUrl = `https://api.spotify.com/v1/recommendations?limit=${length}&market=US&seed_genres=${gymGenre}&target_danceability=${gymDanceability}&min_energy=0.4&target_popularity=${gymPopularity}&min_tempo=120&max_tempo=140&min_valence=.5`;
    return gymUrl;
 };
 
 function getRandomTanRecommendation (length){
-    const tanGenres = ["chill", "idm", "indie-pop", "groove", "hip-hop", "indie-pop", "r-n-b", "road-trip", "summer"];
+    const tanGenres = ["chill", "idm", "indie-pop", "groove", "hip-hop", "indie-pop", "r-n-b", "pop", "road-trip", "summer"];
     let tanGenre = tanGenres[Math.floor(Math.random() * tanGenres.length)];
     let tanDanceability = (Math.random() * (0.2 - .7) + .7).toFixed(1);
-    let tanPopularity = (Math.random() * (50 - 100) + 100).toFixed(0);
-    let tanUrl = `https://api.spotify.com/v1/recommendations?limit=${length}&market=US&seed_genres=pop%2C%20${tanGenre}&target_danceability=${tanDanceability}&min_energy=0.4&target_popularity=${tanPopularity}&min_tempo=100&max_tempo=130&min_valence=.2`;
+    let tanPopularity = (Math.random() * (10 - 100) + 100).toFixed(0);
+    let tanUrl = `https://api.spotify.com/v1/recommendations?limit=${length}&market=US&seed_genres=${tanGenre}&target_danceability=${tanDanceability}&min_energy=0.4&target_popularity=${tanPopularity}&min_tempo=100&max_tempo=130&min_valence=.2`;
     return tanUrl;
   };
   
   function getRandomLaundryRecommendation (length){
-    const laundryGenres = ["acoustic", "chill", "guitar", "happy", "indie", "study"];
+    const laundryGenres = ["acoustic", "chill", "guitar", "happy", "indie", "study", "pop"];
     let laundryGenre = laundryGenres[Math.floor(Math.random() * laundryGenres.length)];
     let laundryDanceability = (Math.random() * (0.1 - .5) + .5).toFixed(1);
-    let laundryPopularity = (Math.random() * (40 - 100) + 100).toFixed(0);
-    let laundryUrl = `https://api.spotify.com/v1/recommendations?limit=${length}&market=US&seed_genres=pop%2C%20${laundryGenre}&target_danceability=${laundryDanceability}&min_energy=0.2&target_popularity=${laundryPopularity}&min_tempo=90&max_tempo=130&min_valence=.2`;
+    let laundryPopularity = (Math.random() * (20 - 100) + 100).toFixed(0);
+    let laundryUrl = `https://api.spotify.com/v1/recommendations?limit=${length}&market=US&seed_genres=${laundryGenre}&target_danceability=${laundryDanceability}&min_energy=0.2&target_popularity=${laundryPopularity}&min_tempo=90&max_tempo=130&min_valence=.2`;
     return laundryUrl
   };
